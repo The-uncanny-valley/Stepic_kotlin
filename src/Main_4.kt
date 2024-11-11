195ace760cc5fef191b8bc915173cc7408df477e
The-uncanny-valley <118472622+Workalina@users.noreply.github.com>
2024-11-11 16:00:03 +0300

    Main_4.kt added

---
+++ src/Main_4.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val number = readln().toInt()
+    val digit1 = number / 100
+    val digit2 = (number / 10) % 10
+    val digit3 = number % 10
+    if (digit1 < digit2 && digit2 < digit3) {
+        println("YES")
+    }
+    else
+    {
+        println("NO")
+    }
+
+}
